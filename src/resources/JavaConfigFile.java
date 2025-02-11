59df375d19f418cd51e14d295536b51f1976b058
ankit-1999-sinha <ankitecesinha@gmail.com>
2025-02-11 09:10:14 +0530

    using constructor

--- src/resources/JavaConfigFile.java
+++ src/resources/JavaConfigFile.java
@@ -10,20 +10,16 @@ public class JavaConfigFile {
 
     @Bean
     public Address createAddress(){
-        Address address = new Address();
-        address.setCity("delhi");
-        address.setPincode(734014);
-        address.setHouseNo(2);
+        Address address = new Address(2,"delhi",734014);
+
         return address;
     }
 
     @Bean
     public Student createStudent(){
 
-        Student student = new Student();
-        student.setAddress(createAddress());
-        student.setName("Ankit");
-        student.setRollNo(55);
+        Student student = new Student(2,"ankit",createAddress());
+
         return student;
     }
 }
