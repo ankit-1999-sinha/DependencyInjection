1bb60c00ac32003daedd70f415bf0614eda48813
ankit-1999-sinha <ankitecesinha@gmail.com>
2025-02-12 08:56:20 +0530

    mode byType and autowire candidate

--- src/beans/Student.java
+++ src/beans/Student.java
@@ -4,7 +4,7 @@ public class Student {
     private int rollNo;
     private String name;
     private Address address; // in mode byName this should be same as beanId and internally it uses setter method
-// in mode byType it checks data type
+// in mode byType it checks data type it also uses setter method
     public void setRollNo(int rollNo) {
         System.out.println("setter method for rollNo");
         this.rollNo = rollNo;
